7a00c1b9d0fd00263f4f3573a1e0e89f0d515b5b
mdirect <muza33@ya.ru>
2025-12-18 17:02:20 +0300

    add all auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router';
-import MainPage from './pages/MainPage/MainPage';
-import Layout from './app/Layout/Layout';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Layout from "./app/Layout/Layout";
+import MainPage from "./pages/MainPage";
+import AuthPage from "./pages/AuthPage";
+import axiosInstance, { setAccessToken } from "./shared/lib/axiosInstance";
 
 function App() {
+  const [user, setUser] = useState({ status: "logging", data: null });
+
+  useEffect(() => {
+    axiosInstance("/api/auth/refreshToken")
+      .then(({ data }) => {
+        setUser({ status: "logged", data: data.user });
+        setAccessToken(data.accessToken);
+      })
+      .catch(() => {
+        setUser({ status: "guest", data: null });
+        setAccessToken("");
+      });
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<MainPage />} />
-          {/* <Route path="/autorization" element={<AutorizationPage />} /> */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout user={user} setUser={setUser} />}>
+            <Route path="/" element={<MainPage user={user} />} />
+            <Route path="/registery" element={<AuthPage setUser={setUser} />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
