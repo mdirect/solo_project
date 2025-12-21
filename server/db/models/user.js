9afe49e3b15872703988b3312da8bb5a4cd3bc3a
mdirect <muza33@ya.ru>
2025-12-21 13:06:29 +0300

    server skelet ready(with postman check)

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -4,7 +4,7 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
-      this.hasMany(models.Dictionary, { foreignKey: 'userId' });
+      this.hasMany(models.Skelet, { foreignKey: 'userId' });
     }
 
     static validateEmail(email) {
--- server/src/configs/server.config.js
+++ server/src/configs/server.config.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 const corsConfig = {
   origin: ['http://localhost:5173'],
@@ -12,6 +13,7 @@ function serverConfig(app) {
   app.use(express.static('public'));
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
+  app.use(bodyParser.json());
   app.use(morgan('dev'));
   app.use(cors(corsConfig));
   app.use(cookieParser());
